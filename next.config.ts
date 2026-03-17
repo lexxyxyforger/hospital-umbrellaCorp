8f206ff45b4b3b3d3ba60b471d61ce9886ac1cad
lexxxyforger <lexyorforger22@gmail.com>
2026-03-17 11:57:19 +0700

    initial commit website

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "randomuser.me", "ui-avatars.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+    ],
   },
 };
 
